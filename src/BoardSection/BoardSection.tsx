2302351dadc7d3a38eed809b7c2af97edbdb8849
emilyfinning <em@emilyjay.co.uk>
2024-02-15 18:05:20 +0000

    board section

--- src/BoardSection/BoardSection.tsx
+++ src/BoardSection/BoardSection.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Props } from "./BoardSection.types";
 import "./BoardSection.css";
 
+import { HiDotsHorizontal } from "react-icons/hi";
+
 const BoardSection = ({ children, title, cardCount }: Props) => {
   return (
     <div className="board-section-container">
-      <div>
-        <div>
-          <h2 className="hey">{title}</h2>
-          <div>{cardCount}</div>
+      <div className="board-section-header">
+        <div className="board-section-header-left">
+          <h2 className="board-section-title">{title}</h2>
+          <div className="board-section-card-count">{cardCount}</div>
         </div>
-        <div></div>
+        <button className="icon-button">
+          <HiDotsHorizontal />
+        </button>
       </div>
       {children}
     </div>
