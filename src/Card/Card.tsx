c18a317bcd4eb02ebcef1e54a7b3b4e613c44974
emilyfinning <em@emilyjay.co.uk>
2024-03-04 18:28:00 +0000

    dragging cards in progress

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Props } from "./Card.types";
 import "./Card.css";
 import Tag from "../Tag/Tag";
+import { useMousePosition } from "../hooks";
+
+const dragStyle = (top: number, left: number, width: string) =>
+  ({
+    position: "absolute",
+    top: top,
+    left: left,
+    width: width,
+  } as React.CSSProperties);
 
 const Card = ({ title, description, tags }: Props) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [cardWidth, setCardWidth] = useState("0px");
+  const card = useRef<HTMLDivElement>();
+  const mousePosition = useMousePosition();
+
+  useEffect(() => {
+    setCardWidth(`${card.current.clientWidth}px`);
+  }, [card.current]);
+
   return (
-    <div className="card-container" aria-role="button">
+    <div
+      onMouseDown={() => setIsDragging(true)}
+      onMouseUp={() => setIsDragging(false)}
+      className="card-container"
+      aria-role="button"
+      style={
+        isDragging ? dragStyle(mousePosition.y, mousePosition.x, cardWidth) : {}
+      }
+      ref={card}
+    >
       <h3 className="card-title">{title}</h3>
       <p className="card-description">{description}</p>
       <div className="card-tags">
--- src/Tag/Tag.tsx
+++ src/Tag/Tag.tsx
@@ -3,7 +3,6 @@ import { Props } from "./Tag.types";
 import "./Tag.css";
 
 const Tag = ({ color, text }: Props) => {
-  console.log(color, text);
   return (
     <div className="tag-container">
       <div className="tag-dot" style={{ backgroundColor: color }} />
